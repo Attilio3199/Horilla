ec19c07e3f9cd71ae646a6af4b7ec87e06bf2357
secondary <secondary@secondary.secondary.appsmith.gioiellidivalenza.com>
2026-05-04 16:49:41 +0200

    fix per docker e postgresql su docker

--- horilla/settings.py
+++ horilla/settings.py
@@ -169,7 +169,7 @@ STATICFILES_DIRS = [
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+MEDIA_ROOT = env.str("MEDIA_ROOT", default=os.path.join(BASE_DIR, "media/"))
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
